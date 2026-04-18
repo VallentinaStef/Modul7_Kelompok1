dbdbf72cf80ae0c05f79827f65df578f812185ab
unknown <redflare31138@gmail.com>
2026-04-18 09:19:05 +0700

    Json 2

--- Modul7_Kelompok1/Program.cs
+++ Modul7_Kelompok1/Program.cs
@@ -9,6 +9,7 @@ class Program
     {
 
         ReadJSON();
+        ReadJSON2();
     }
     public static void ReadJSON() 
     {
@@ -16,6 +17,20 @@ class Program
         string json = File.ReadAllText("D:\\Modul7_Kelompok1\\Modul7_Kelompok1\\jurnal7_1_103022400002.json");
         var data = JsonSerializer.Deserialize<FilmFavorite_103022430013_dwi>(json);
         Console.WriteLine(data);
+    }
+
+    public static void ReadJSON2()
+    {
+        string json = File.ReadAllText("D:\\Modul7_Kelompok1\\Modul7_Kelompok1\\jurnal7_2_103022400002.json");
+        var film = JsonSerializer.Deserialize<Watchlist_103022430013_dwi>(json);
+        Console.WriteLine($"Watchlist Name :{film.watchlistName}");
+        Console.WriteLine($"Created By :{film.createdBy}");
+        Console.WriteLine($"Movie :");
+
+        foreach (var films in film.movie)
+        {
+            Console.WriteLine(films.id + " " + films.title + " " + films.year);
+        }
     }
 }
 
---
+++ Modul7_Kelompok1/Watchlist_103022430013_dwi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul7_Kelompok1
+{
+    internal class Watchlist_103022430013_dwi
+    {
+        public string watchlistName {  get; set; }
+        public string createdBy { get; set; }
+        public List<movies> movie  { get; set; }
+    }
+}
+public class movies
+{
+    public string id { get; set; }
+    public string title { get; set; }
+    public int year { get; set; }
+    public string genre { get; set; }
+    public double rating{ get; set; }
+}
