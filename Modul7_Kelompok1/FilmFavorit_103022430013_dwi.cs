b845554c999addc8612fc9522114870c57fd7865
unknown <redflare31138@gmail.com>
2026-04-18 08:48:24 +0700

    menambahkan class FilmFavorite_103022430013_dwi

---
+++ Modul7_Kelompok1/FilmFavorit_103022430013_dwi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul7_Kelompok1
+{
+    internal class FilmFavorite_103022430013_dwi
+    {
+        public string title { get; set; }
+        public string year { get; set; }
+        public string director { get; set; }
+        public string genre { get; set; }
+        public int rating { get; set; }
+        public int durationMinutes { get; set; }
+        public bool isWatched { get; set; }
+        
+    }
+}
--- Modul7_Kelompok1/Program.cs
+++ Modul7_Kelompok1/Program.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 using Modul7_Kelompok1;
 class Program
 {
-    public static void main(String[] args)
+    public static void Main(String[] args)
     {
 
+        ReadJSON();
+    }
+    public static void ReadJSON() 
+    {
+
+        string json = File.ReadAllText("D:\\Modul7_Kelompok1\\Modul7_Kelompok1\\jurnal7_1_103022400002.json");
+        var data = JsonSerializer.Deserialize<FilmFavorite_103022430013_dwi>(json);
+        Console.WriteLine(data);
     }
 }
+
+
