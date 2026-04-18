54e378c47cf3eaf044086edab665c9df19664d35
IlhamAlifAryasatya <ialif9693@gmail.com>
2026-04-18 08:52:09 +0700

    second commit

---
+++ Modul7_Kelompok1/FilmFavorit_103022430011.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class FilmFavorit_103022430011
+{
+
+    public string title { get; set; }
+    public string director { get; set; }
+    public string year { get; set; }
+    public string genre { get; set; }
+    public int rating { get; set; }
+    public int durationMinutes { get; set; }
+    public Boolean isWatched { get; set; }
+
+    public void ReadJSON()
+    {
+        string json = File.ReadAllText("jurnal7_1_103022430011.json");
+        FilmFavorit_103022430011? data = JsonSerializer.Deserialize<FilmFavorit_103022430011>(json);
+
+        if (data != null )
+        {
+            Console.WriteLine($"Judul film adalah {data.title}, direktor filmnya {data.director}, rilis tahun {data.year}, genrenya {data.genre}, ratingnya {data.rating}, durasi filmnya {data.durationMinutes}, filmnya sudah ditonton?(true/false) {data.isWatched} ");
+        }
+    }
+}
--- Modul7_Kelompok1/Program.cs
+++ Modul7_Kelompok1/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Modul7_Kelompok1;
+
 class Program
 {
-    public static void main(String[] args)
+    public static void Main(String[] args)
     {
-
+        FilmFavorit_103022430011 film = new FilmFavorit_103022430011();
+        film.ReadJSON();
     }
 }
