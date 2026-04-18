ec37b749996ff11ec9814c535aa5962c1f0db18d
ilhamrmdhn <ilham712svr@gmail.com>
2026-04-18 08:49:17 +0700

    menambahkan class FilmFavorit_103022400015

---
+++ Modul7_Kelompok1/FilmFavorit_103022400015.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Modul7_Kelompok1
+{
+    internal class FilmFavorit_103022400015
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+        public string year { get; set; }
+        public string genre { get; set; }
+        public int rating { get; set; }
+        public int durationMinutes { get; set; }
+        public Boolean isWatched { get; set; }
+
+        
+    }
+}
--- Modul7_Kelompok1/Program.cs
+++ Modul7_Kelompok1/Program.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Modul7_Kelompok1;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using Modul7_Kelompok1;
+using System.Text.Json;
 class Program
 {
-    public static void main(String[] args)
+    public static void Main(String[] args)
     {
+        ReadJSON();
+    }
 
+    public static void ReadJSON()
+    {
+        string jsonString = File.ReadAllText("D:\\Modul7_Kelompok1\\Modul7_Kelompok1\\jurnal7_1_103022400015.json");
+        var data = JsonSerializer.Deserialize<FilmFavorit_103022400015>(jsonString);
+        Console.WriteLine(data);
     }
 }
